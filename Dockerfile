cd5b12271dbb5af03eb9fc65fbb19ee2c677f131
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-06-28 03:23:46 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM openjdk:11.0-jre-slim
+
+ENV VERSION="1.15.2" \
+    BUILD="latest"
+
+ADD paper.sh .
+RUN apt-get update \
+    && apt-get install -y wget \
+    && rm -rf /var/lib/apt/lists/*
+
+CMD ["sh", "./paper.sh"]
+
+EXPOSE 25565/tcp
+EXPOSE 25565/udp
+VOLUME /data
---
+++ paper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p papermc
+# shellcheck disable=SC2164
+cd papermc
+
+JAR_NAME=papermc-${VERSION}-${BUILD}
+
+if [ ! -e "${JAR_NAME}".jar ] || [ "${BUILD}" = latest ]
+  then
+    wget https://papermc.io/api/v1/paper/"${VERSION}"/"${BUILD}"/download -O "${JAR_NAME}".jar
+    if [ ! -e eula.txt ]
+    then
+      java -jar "${JAR_NAME}".jar
+      sed -i 's/false/true/g' eula.txt
+    fi
+fi
+
+java -server -jar "${JAR_NAME}".jar nogui
